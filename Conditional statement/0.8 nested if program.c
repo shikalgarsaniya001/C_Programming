6f452a824736d6a02c45904e4cb8c3b7fdc79109
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2024-10-10 07:32:25 +0530

    Nested if

---
+++ Conditional statement/0.8 nested if program.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int num;
+    printf("\n Enter a number :");
+    scanf("\n %d",& num);
+    if(num>=0)
+    {
+          printf("\n positive number");
+
+         if(num%2==0)
+       {
+           printf("\n Even number ");
+       }
+       else
+       {
+           printf("\n Odd number ");
+       }
+
+
+    }
+
+    else
+
+        printf("\n negative number");
+        {
+               if(num%2==0)
+       {
+           printf("\n Even number ");
+       }
+       else
+       {
+           printf("\n Odd number ");
+       }
+
+        }
+
+
+    getch();
+    return 0;
+}
+
+
