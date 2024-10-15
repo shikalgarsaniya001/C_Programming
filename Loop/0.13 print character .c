6a7ed9c484fc37f44ca929e47ed02cf601cf9366
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2024-10-15 06:10:35 +0530

    0.13 character print program .c

---
+++ Loop/0.13 print character .c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char ch='a';
+    while(ch<='z')
+    {
+        printf("\n %c",ch);
+            ch++;
+    }
+    getch();
+    return 0;
+}
+
