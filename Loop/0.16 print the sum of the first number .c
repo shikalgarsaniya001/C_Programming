9c5ed145896adac53b16bb1998af15ae16885440
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2024-10-17 08:02:54 +0530

    Print sum of the first number .c

---
+++ Loop/0.16 print the sum of the first number .c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int n;
+    printf("\n Enter a number :");
+    scanf("\n %d",&n);
+    int i,sum;
+    for(i=0; i<=n; i++)
+    {
+        printf("\n %d",i);
+    }
+    sum=sum+i;
+    printf("\n %d",sum);
+    getch();
+    return 0;
+}
