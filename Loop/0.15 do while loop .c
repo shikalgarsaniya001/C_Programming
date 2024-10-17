6160be2feb2d1bf2277021130dcba8fb0043100a
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2024-10-17 08:01:51 +0530

    Use do while loop .c

---
+++ Loop/0.15 do while loop .c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i=5;
+    do
+    {
+        printf("\n %d",i);
+        i--;
+    }
+    while(i>=1);
+    getch();
+    return 0;
+}
+
