7eec76bdc324af5a408bc98e49a7c3c99e023247
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2024-10-18 19:18:19 +0530

    0.17 print sum of the reverce number .c

---
+++ Loop/0.17 print sum of the reverce number .c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i=0,sum=0,n=0;
+    printf("\n Enter a number :");
+    scanf("\n %d",&n);
+    for(i=n; i>=1;i--)
+    {
+        printf("\n %d",i);
+    }
+
+    getch();
+    return 0;
+
+}
