1fbbce4c65c88480561873d980391bef31f70a61
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2024-10-18 19:15:55 +0530

    0.13 print number .c

---
+++ Loop/0.13 print number user defin .c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int n;
+    printf("\n Enter a number :");
+    scanf("\n %d",&n);
+    int i;
+    for(i=0; i<=n; i++)
+    {
+        printf("\n %d",i);
+    }
+    getch();
+    return 0;
+}
