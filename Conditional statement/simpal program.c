fae85d28766c2d69f49a946f7a89b86f0c6bb19a
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2024-10-25 07:45:28 +0530

    simple

---
+++ Conditional statement/simpal program.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdio.h>
+int main()
+{
+    int num=10;
+    printf("\n Welcome");
+    if(num<100)
+    {
+        printf("\n saniya");
+        printf("\n fork");
+
+    }
+    printf("\n thank you");
+    getch();
+    return 0;
+}
