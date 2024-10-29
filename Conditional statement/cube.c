f02a65d47205e3a365eed0c4ef892df1a18e9b3b
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2024-10-29 09:03:14 +0530

    simp;e

---
+++ Conditional statement/cube.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int no=0, cube=0;
+printf("enter any number:");
+scanf("%d",&no);
+cube=no*no*no;
+printf("cube of %d is = %d",no,cube);
+getch();
+return 0;
+}
