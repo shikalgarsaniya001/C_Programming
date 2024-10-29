fbdd74da0f78b528c6e7432189afe40a4954386f
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2024-10-29 09:00:47 +0530

    simple

---
+++ Conditional statement/sqr.c
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int saniya=0, sqr=0;
+    printf("enter any number:");
+    scanf("%d",&saniya);
+    sqr=saniya*saniya;
+    printf("sqr of %d is = %d",saniya,sqr);
+    getch();
+    return 0;
+}
