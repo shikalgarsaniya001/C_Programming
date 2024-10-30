dcf3169eb01527ac089db526d83ac940e4ed08a6
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2024-10-30 19:32:49 +0530

    Sub two number

---
+++ Practice Q/SUB of two number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a,b;
+    printf("\n Enter a number a :");
+    scanf("%d",&a);
+    printf("\n Enter a number b :");
+    scanf("\n %d",&b);
+
+     printf("\n Addition of given number %d",a-b);
+
+      return 0;
+}
+
