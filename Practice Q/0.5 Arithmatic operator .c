303be42c6f56bf75caf74458e868e743ca5bf0a2
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2024-11-05 09:27:34 +0530

    Arithmatic .c

---
+++ Practice Q/0.5 Arithmatic operator .c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a= 5*2 / 2*3 ;
+    printf("\n -----------------------------");
+
+    printf("\n %d",a);
+
+    printf("\n -----------------------------");
+
+    return 0;
+
+
+}
+
+
