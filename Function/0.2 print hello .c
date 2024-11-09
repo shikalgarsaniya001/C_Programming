c5a3d8fe122170d0215e376059dee17c6d4e7d53
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2024-11-09 20:03:04 +0530

    Hello

---
+++ Function/0.2 print hello .c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void printHello();
+int main()
+{
+    printHello();
+    return 0;
+}
+void printHello()
+{
+    printf("\n hello");
+    printf("\n saniya");
+    printf("\n How are you");
+}
