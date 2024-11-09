86223917c8579a5a523bc4ce8c32dc1af1eff5db
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2024-11-09 20:12:36 +0530

    Simple

---
+++ Function/0.3 print hello .c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void printHello();
+int main()
+{
+    printHello();
+
+    printHello();
+
+    printHello();
+
+    printHello();
+
+}
+ void printHello()
+ {
+     printf("\n hello");
+     printf("\n I am saniya");
+     printf("\n I come from shenoli \n");
+ }
