c75ff3eee4ecdedc5ab059b20990ab3105366960
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2024-11-15 21:08:25 +0530

    Sub

---
+++ Function/Sub of the two number .c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+int Sub(int N1, int N2)
+{
+    int Ans=0;
+    Ans = N1 - N2;
+    return Ans;
+}
+
+
+int main()
+{
+    int Num1 = 0, Num2 = 0, Res = 0;
+    printf("\n Enter a first  Number  =  ");
+    scanf("%d",&Num1);
+    printf("\n Enter a secound number = ");
+    scanf("%d",&Num2);
+    Res = Sub(Num1 , Num2);
+    printf("\n SUB of %d and %d is = %d",Num1,Num2,Res);
+
+
+    getch();
+    return 0;
+
+}
+
