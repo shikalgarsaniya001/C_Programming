d98a8c1d1780fd897c0ac463fe2f14a84cb990d5
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-01-16 13:58:40 +0530

    Array

---
+++ Array/Array/0.9 Array .c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int iNum[5];
+    printf("\n value of element 01 <%d>",iNum[0]);
+    printf("\n value of element 02 <%d>",iNum[1]);
+    printf("\n value of element 03<%d>",iNum[2]);
+    printf("\n value of element 04 <%d>",iNum[3]);
+    printf("\n value of element 05 <%d>",iNum[4]);
+    getch();
+
+    return 0;
+
+}
