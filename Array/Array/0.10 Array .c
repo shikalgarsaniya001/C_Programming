1889691600e120af39690dc097ce4c8f18c32304
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-01-16 13:59:39 +0530

    Array

---
+++ Array/Array/0.10 Array .c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int iNum[5]={0,0,0,0,0};
+
+    printf("\n value of 1 element %d",iNum[0]);
+    printf("\n value of 2 element %d",iNum[1]);
+    printf("\n value of 3 element %d",iNum[2]);
+    printf("\n value of 4 element %d",iNum[3]);
+    printf("\n value of 5 element %d",iNum[4]);
+
+    getch();
+    return 0;
+
+}
