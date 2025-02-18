aa39902e31cec363a41873eccb140dd8599cebd8
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-02-18 12:02:28 +0530

    Array.c

---
+++ Array/Array/Create Array of 7 element with null(0) initialization & Accept &all element without loop display all elements.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+int main()
+{
+    int Bill[5]={0};
+    printf("\n Accept bill without loop ");
+    printf("\n Enter Bill 1 : ");
+    scanf("%d",&Bill[0]);
+    printf("\n Enter Bill 2 : ");
+    scanf("%d",&Bill[1]);
+    printf("\n Enter Bill 3 : ");
+    scanf("%d",&Bill[2]);
+    printf("\n Enter Bill 4 : ");
+    scanf("%d",&Bill[3]);
+    printf("\n Enter Bill 5 : ");
+    scanf("%d",&Bill[4]);
+    printf("\n Enter Bill 6 : ");
+    scanf("%d",&Bill[5]);
+    printf("\n Enter Bill 7 : ");
+    scanf("%d",&Bill[6]);
+
+    getch();
+    system("cls");
+    printf("\n Value of bill is 1 = %d",Bill[0]);
+    printf("\n Value of bill is 2 = %d",Bill[1]);
+    printf("\n Value of bill is 3 = %d",Bill[2]);
+    printf("\n Value of bill is 4 = %d",Bill[3]);
+    printf("\n Value of bill is 5 = %d",Bill[4]);
+    printf("\n Value of bill is 6 = %d",Bill[5]);
+    printf("\n Value of bill is 7 = %d",Bill[6]);
+
+     getch();
+    return 0;
+
+}
