7c121f01e8f97a9c92f61870340f9e133a53a10e
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-02-19 22:19:41 +0530

    loop.c

---
+++ Loop/0.16 sum of the number .c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i=0,sum=0;
+    for(int i=0;i<=4;i++)
+    {
+        sum=sum+i;
+        printf("\n sum of %d",sum);
+    }
+    getch();
+    return 0;
+}
