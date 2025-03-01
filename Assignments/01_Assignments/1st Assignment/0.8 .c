536569d5614656e87fdbb532807a3a213af835fc
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-03-01 11:36:11 +0530

    ASCII value for given character

---
+++ Assignments/01_Assignments/1st Assignment/0.8 .c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+int main()
+{
+
+   char ch ='/0';
+   printf("\n Enter a character :");
+   scanf("\n %c",&ch);
+
+   printf("\n ASCII value for Alphabets %c is %d ",ch, ch);
+
+   printf("\n Thank you");
+
+    return 0;
+
+
+}
