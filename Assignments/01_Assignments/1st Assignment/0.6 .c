0bca9227596765d2d47bd883be467db3b8f7de0f
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-03-01 11:34:23 +0530

    print size

---
+++ Assignments/01_Assignments/1st Assignment/0.6 .c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char a;
+    int b;
+    float c;
+    double d;
+
+    printf("\n  size of char  : %d",sizeof(a));
+    printf("\n  size of int   : %d",sizeof(b));
+    printf("\n  size of float : %d",sizeof(c));
+    printf("\n  size of double: %d",sizeof(d));
+
+    getch();
+    return ;
+
+
+}
+
