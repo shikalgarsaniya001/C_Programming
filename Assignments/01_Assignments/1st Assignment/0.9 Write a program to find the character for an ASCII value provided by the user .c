a4bfb9cab5f0bf3f81b36533305afd8c5cb1f459
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-03-01 11:40:43 +0530

    Find the character for ASCII value

---
+++ Assignments/01_Assignments/1st Assignment/0.9 Write a program to find the character for an ASCII value provided by the user .c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int Num =0;
+
+    printf("\n  Enter ASCII value :");
+    scanf("%d",&Num);
+
+    printf("The character for ASCII value %d = %c",Num,Num);
+
+    return 0;
+
+}
+
+
+
