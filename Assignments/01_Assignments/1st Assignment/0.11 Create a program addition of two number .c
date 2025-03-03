b0886efb26f22b973975f8cdd45aa36f56d3cd32
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-03-03 12:24:56 +0530

    Calculate its Area

---
+++ Assignments/01_Assignments/1st Assignment/0.11 Create a program addition of two number .c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+ int No1,No2 ,sum;
+ printf("Enter a first number:");
+ scanf("%d",&No1);
+ printf("Enter a second number :");
+ scanf("%d",&No2);
+
+ sum=No1+No2;
+ printf("Addition of two number %d",sum);
+ getch();
+ return 0;
+
+}
