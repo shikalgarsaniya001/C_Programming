6c011116f71433ad90596b44f623e2624dada3e8
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-03-03 12:27:48 +0530

    calculate its Area

---
+++ Assignments/01_Assignments/1st Assignment/0.13 .C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    float width, height, Area;
+
+    printf("\n Enter a width :");
+    scanf("%f",&width);
+
+    printf("\n Enter a height :");
+    scanf("%f",&height);
+
+    Area = width * height;
+
+    printf("\n Area is %f",Area);
+
+
+    return 0;
+
+
+}
