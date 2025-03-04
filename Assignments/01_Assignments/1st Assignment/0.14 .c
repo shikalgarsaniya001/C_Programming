9f9da320944cc7d57726edec0e64e087e0141d48
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-03-04 08:43:53 +0530

    Calculates its area

---
+++ Assignments/01_Assignments/1st Assignment/0.14 .c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int side, Area ;
+
+    printf("Enter a side :");
+    scanf("%d",&side );
+
+     Area = side * side;
+
+     printf("Area of square %d ",Area);
+
+     getch();
+     return 0;
+}
+
+
