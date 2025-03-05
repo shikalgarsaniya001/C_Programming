a8cd34c148c678a8a6b2b05a9947c50ab59bb6ad
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-03-05 11:14:39 +0530

    Calculate the square of that number

---
+++ Assignments/01_Assignments/1st Assignment/0.17 .c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int Num , Square;
+    printf("\n *************************");
+
+    printf("\n Enter a number :");
+    scanf("%d",&Num );
+
+
+    Square  = Num* Num;
+
+    printf(" square of the number %d ",Square);
+
+    printf("\n *************************");
+
+     getch();
+     return 0;
+}
+
+
+
