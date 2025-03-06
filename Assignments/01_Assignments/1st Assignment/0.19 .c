5811a2d4fe9e8e04173477d9a1a54b9ba6e9a1ac
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-03-06 08:30:08 +0530

    Calculate the square root of that number

---
+++ Assignments/01_Assignments/1st Assignment/0.19 .c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int Num = 0, sRoot;
+    printf("\n *************************");
+
+    printf("\n Enter a number :");
+    scanf("%d",&Num );
+
+   sRoot = sqrt(Num);
+
+    printf("\n SquareRoot is %d",sRoot);
+
+    printf("\n *************************");
+
+     getch();
+     return 0;
+}
+
+
+
+
