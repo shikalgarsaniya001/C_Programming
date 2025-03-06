cff88f8f1e871fa4efd37943cf0f27f569a1c4c4
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-03-06 08:28:30 +0530

    Calculate the cube of number

---
+++ Assignments/01_Assignments/1st Assignment/0.18 .c
@@ -0,0 +1,23 @@
+
+#include<stdio.h>
+int main()
+{
+    int Num , Cube;
+    printf("\n *************************");
+
+    printf("\n Enter a number :");
+    scanf("%d",&Num );
+
+
+    Cube = Num* Num * Num;
+
+    printf(" Cube of the number %d ",Cube);
+
+    printf("\n *************************");
+
+     getch();
+     return 0;
+}
+
+
+
