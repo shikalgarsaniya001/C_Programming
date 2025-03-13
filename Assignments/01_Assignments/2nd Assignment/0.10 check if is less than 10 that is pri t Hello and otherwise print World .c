b0c0fd9ac8b7128beaaa0864d3c9666969cd122f
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-03-13 06:45:34 +0530

    if else program

---
+++ Assignments/01_Assignments/2nd Assignment/0.10 check if is less than 10 that is pri t Hello and otherwise print World .c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int No;
+    printf("\n Enter a number :");
+    scanf("%d",&No);
+    if(No<10)
+    {
+        printf("\n Hello");
+    }
+    else
+    {
+        printf("\n World");
+    }
+    getch();
+    return 0;
+}
+
