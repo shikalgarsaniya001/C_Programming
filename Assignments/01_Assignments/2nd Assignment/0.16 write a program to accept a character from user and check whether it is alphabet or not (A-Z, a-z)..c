a34ed9dd4ed371a10d6c07f0daeb00d2150b9c7b
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-03-15 08:05:45 +0530

    check whether it is alphabet or not

---
+++ Assignments/01_Assignments/2nd Assignment/0.16 write a program to accept a character from user and check whether it is alphabet or not (A-Z, a-z)..c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char ch;
+
+    printf("\n Enter a character :");
+    scanf("%c",&ch);
+
+    if(ch>='A'&& ch<='Z'|| ch>='a' && ch<='z')
+    {
+        printf("\n TRUE ");
+    }
+
+    else
+    {
+        printf("\n FALSE");
+    }
+    getch();
+    return 0;
+}
+
+
